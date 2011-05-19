456d333714bb01763a2f7ad1152de7a6f14ca6a3
adamfinden <adam@adamfinden.com>
2011-05-19 20:03:52 +1000

    added cutup structure
    Signed-off-by: adamfinden <adam@adamfinden.com>

---
+++ core-structure/js/jquery.textresizzle.js
@@ -0,0 +1,106 @@
+/**
+ * Text Resizzle	
+ * For resizing text by attaching specific classes to a parent element. 
+ * For more information, demos etc, see the page in confluence. 
+ * @author Henry Singleton
+ * @date Nov 3 2010
+ */
+
+(function($) {
+	$.fn.textResizzle = function(options) {
+		
+		var settings = {
+			parent: '#primary',				// select the parent to apply the size classes to
+			increaseSizeSelector: '.text-increase', // select element to use for increase font size button or link. 
+			decreaseSizeSelector: '.text-decrease', // select element to use for decrease font size button or link. 
+			sizes: [					// specify an array of the sizes. These will be applied as class names to the parent element (selected above) to allow you to overide layout and font sizes. 
+				'smallest',
+				'small',
+				'normal',
+				'large',
+				'largest'
+			],
+			defaultSize: 'normal', 		// specify which size above is the default. 
+			saveCookie: true,			// choose weather to save the selected font size in a cookie
+			cookieName: 'textResizzle'
+		};
+		
+		if (options) {
+			$.extend(settings,options);
+		}
+		
+		//set the default size
+		var currentSize = settings.defaultSize;
+		
+		//set up our container
+		var parent = $(settings.parent);
+			
+		// set up the increase and decrease selectors
+		
+		$(settings.increaseSizeSelector).click(increaseSize);
+		$(settings.decreaseSizeSelector).click(decreaseSize);
+			
+		//set the size to whatever is stored in the current cookie
+		if ($.cookies.get(settings.cookieName)) {
+			setSize($.cookies.get(settings.cookieName));
+		}
+
+		function increaseSize() {
+			
+			/* alert(jQuery.inArray(currentSize, settings.sizes)); */
+			//get the next size up index
+			var nextSize = jQuery.inArray(currentSize, settings.sizes) + 1;
+	
+			//check if we have reached the top of the sizing.
+			if (nextSize >= settings.sizes.length) {
+				return false;
+			}
+			
+			//set the new size
+			setSize(settings.sizes[nextSize]);
+			
+			return false;
+		}
+		
+		function decreaseSize() {
+			//get the next size up index
+			var previousSize = jQuery.inArray(currentSize, settings.sizes) - 1;
+	
+			//check if we have reached the top of the sizing.
+			if (previousSize < 0) {
+				return false;
+			}
+			
+			//set the new size
+			setSize(settings.sizes[previousSize]);
+			
+			return false;
+	
+		}
+		
+		//explicitly set the size. Will normally only be called internally. 
+		function setSize(size) {
+			
+			if (typeof size == undefined) {
+				size = settings.defaultSize;
+			}
+			
+			parent.removeClass(currentSize);
+			
+			//set the new size
+			parent.addClass(size);
+			
+			//write the new size to the current object
+			currentSize = size;
+			
+			//write the change to the cookie
+			if (settings.saveCookie) {
+				$.cookies.set(settings.cookieName, size, { path: '/'});
+			}
+			
+			//Cufon.refresh();
+		}
+		
+		return this;
+	}	
+})(jQuery);
---
+++ core-structure/js/site.js
@@ -0,0 +1,154 @@
+$(document).ready(function(){
+	
+	Site.init();
+	
+});
+
+var Site = {
+	
+	init:function(sliderSettings) {
+		
+		
+		Site.grid();
+		
+		//HANDLE EXTERNAL LINKS
+		Site.externalLinks();
+				
+	
+		//SEARCH
+		if($('#search').length != 0) {
+			Site.search();
+		}
+	
+				
+		//TEXT RESIZZLE
+		if($('#usability-controls').length != 0) {
+			$('#primary-inner').textResizzle();	
+		}
+		
+		//PRINT
+		if($('#usability-controls').length != 0) {
+			Site.print();
+		}
+		
+		//IMAGE GALLERY
+		if($('#image-gallery').length != 0) {
+			Site.fancyBoxInit();
+			$('#image-gallery li:nth-child(5n)').addClass('nth-child-fifth');
+		
+		}
+		
+		//FORM VALIDATION
+		if($('#membership-application-form').length != 0){
+			$('#member-application').validate();
+		}
+		
+		//CONTACT FORM VALIDATION
+		if($('#contact-form').length != 0) {
+			$('#contact-form').validate();
+		}
+
+	},
+	
+	grid:function() {
+		var body = $('body');
+	
+		
+		body.append('<div id="grid-png" style="height: 100%; width: 100%; position: absolute; top: 0; left: 0; z-index: 9999; "><div class="container_16" style="height: 100%; background: url(development/grids/grid16-lh20.png) repeat-y 0 0;"></div></div>');
+				
+	    body.dblclick(function () { 
+	      $("#grid-png").toggle(); 
+	    });
+		
+		
+		    		
+	},
+
+	memberLogin:function() {
+  
+        $("#member-login").click(function(e) {
+            e.preventDefault();
+            $("#member-login-menue").toggle();
+            $(this).toggleClass("menu-open");
+        });
+
+        $("#member-login-menue").mouseup(function() {
+            return false
+        });
+        
+        $(document).mouseup(function(e) {
+            if($(e.target).parent("#member-login").length==0) {
+                $("#member-login").removeClass("menu-open");
+                $("#member-login-menue").hide();
+            }
+        });            
+
+	},
+	
+	search:function() {
+		
+		$('#search input').focus(function() {
+			$(this).css({'background-position':'-280px -28px'});			
+			$(this).prev('label').css({'left':'-9999px'});
+		});
+		
+		$('#search input').blur(function() {
+			$(this).css({'background-position':'-280px 0'});
+			if ($(this).val() == '')
+				$(this).prev('label').css({'left':'5px'});
+		});
+		
+	},
+	
+	featureFader:function() {
+		$('#user_interaction').cycle({
+			fx: 'fade', 
+			/* pager: '#pagination', */
+			timeout: 5000,
+			pause:   1,
+			next: '#next2',
+			prev: '#prev2'
+		});
+	},
+	
+	providerSlider:function() {
+		$('#providers-carousel ul').cycle({
+			fx: 'scrollLeft', 
+			/* pager: '#pagination', */
+			timeout: 5000,
+			pause:   1,
+			speed:   500,
+			next: '#next3',
+			prev: '#prev3'
+		});
+	},
+	
+	print:function() {
+		
+		$('#print-page').click(function(){
+			window.print();
+		});
+		
+	},
+	
+	externalLinks:function() {
+	
+		var extLinks = $('a.external');
+			if ( extLinks.length ) {
+				extLinks.each(function() { 
+					$(this).attr('target', '_blank');
+			});
+		}
+	
+	},
+	
+	fancyBoxInit:function(){
+
+		$("a[rel=gallery]").fancybox({
+			'titleShow'			: 'False',
+			'overlayColor'		: '#000000',
+			'overlayOpacity'	: '.7'
+		});
+	}
+
+};
