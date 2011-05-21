33213c9cea59fb776dd1d74bc187551fe45761ab
adamfinden <adam@adamfinden.com>
2011-05-21 14:17:07 +1000

    Added error messages for forms, and added image gallery with shadow box
    Signed-off-by: adamfinden <adam@adamfinden.com>

--- core-structure/js/site.js
+++ core-structure/js/site.js
@@ -1,26 +1,37 @@
 $(document).ready(function(){
 	
-	Site.init();
+    Site.init();
+		
+	Shadowbox.init({
+	    overlayOpacity: 0.8
+	}, setupShadowbox);
 	
 });
 
+function setupShadowbox() {
+
+    Shadowbox.setup("a.test-gallery", {
+        gallery:        "Gallery",
+        continuous:     true,
+        counterType:    "skip"
+    });
+
+}
+
 var Site = {
 	
-	init:function(sliderSettings) {
+	init:function() {
 		
-		
-		Site.grid();
-		
+				
 		//HANDLE EXTERNAL LINKS
 		Site.externalLinks();
-				
+
 	
 		//SEARCH
 		if($('#search').length != 0) {
 			Site.search();
 		}
-	
-				
+		
 		//TEXT RESIZZLE
 		if($('#usability-controls').length != 0) {
 			$('#primary-inner').textResizzle();	
@@ -31,12 +42,6 @@ var Site = {
 			Site.print();
 		}
 		
-		//IMAGE GALLERY
-		if($('#image-gallery').length != 0) {
-			Site.fancyBoxInit();
-			$('#image-gallery li:nth-child(5n)').addClass('nth-child-fifth');
-		
-		}
 		
 		//FORM VALIDATION
 		if($('.val-form').length != 0){
@@ -52,39 +57,15 @@ var Site = {
 	
 	grid:function() {
 		var body = $('body');
-	
-		
+
 		body.append('<div id="grid-png" style="height: 100%; width: 100%; position: absolute; top: 0; left: 0; z-index: 9999; "><div class="container_16" style="height: 100%; background: url(development/grids/grid16-lh20.png) repeat-y 0 0;"></div></div>');
 				
 	    body.dblclick(function () { 
 	      $("#grid-png").toggle(); 
 	    });
-		
-		
-		    		
+    		
 	},
 
-	memberLogin:function() {
-  
-        $("#member-login").click(function(e) {
-            e.preventDefault();
-            $("#member-login-menue").toggle();
-            $(this).toggleClass("menu-open");
-        });
-
-        $("#member-login-menue").mouseup(function() {
-            return false
-        });
-        
-        $(document).mouseup(function(e) {
-            if($(e.target).parent("#member-login").length==0) {
-                $("#member-login").removeClass("menu-open");
-                $("#member-login-menue").hide();
-            }
-        });            
-
-	},
-	
 	search:function() {
 		
 		$('#search input').focus(function() {
@@ -99,30 +80,7 @@ var Site = {
 		});
 		
 	},
-	
-	featureFader:function() {
-		$('#user_interaction').cycle({
-			fx: 'fade', 
-			/* pager: '#pagination', */
-			timeout: 5000,
-			pause:   1,
-			next: '#next2',
-			prev: '#prev2'
-		});
-	},
-	
-	providerSlider:function() {
-		$('#providers-carousel ul').cycle({
-			fx: 'scrollLeft', 
-			/* pager: '#pagination', */
-			timeout: 5000,
-			pause:   1,
-			speed:   500,
-			next: '#next3',
-			prev: '#prev3'
-		});
-	},
-	
+		
 	print:function() {
 		
 		$('#print-page').click(function(){
@@ -140,15 +98,6 @@ var Site = {
 			});
 		}
 	
-	},
-	
-	fancyBoxInit:function(){
-
-		$("a[rel=gallery]").fancybox({
-			'titleShow'			: 'False',
-			'overlayColor'		: '#000000',
-			'overlayOpacity'	: '.7'
-		});
 	}
 
 };
